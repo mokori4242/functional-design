eff56ef915f1a08789d7d3019a4a36f21dd20c04
mokori4242 <mokori4242@gmail.com>
2024-08-07 08:43:32 +0900

    [update]マルチメソッド

--- copy/spec/core_spec.clj
+++ copy/spec/core_spec.clj
@@ -3,8 +3,9 @@
             [copy.core :refer :all]))
 
 (describe "copy"
-          (it "can read and write using str-read and str-write"
-              (reset! str-in "abcedf")
-              (reset! str-out "")
-              (copy {:getchar str-read :putchar str-write})
-              (should= "abcedf" @str-out)))
+          (it "can read and write using multi-method"
+              (let [device {:device-type :test-device
+                            :input (atom "abcdef")
+                            :output (atom nil)}]
+                (copy device)
+                (should= "abcdef" @(:output device)))))
--- copy/src/copy/core.clj
+++ copy/src/copy/core.clj
@@ -1,11 +1,14 @@
 (ns copy.core)
 
+(defmulti getchar (fn [device] (:device-type device)))
+(defmulti putchar (fn [device c] (:device-type device)))
+
 (defn copy [device]
-  (let [c ((:getchar device))]
+  (let [c (getchar device)]
     (if (= c :eof)
       nil
       (do
-        ((:putchar device) c)
+        (putchar device c)
         (recur device)))))
 
 (def str-in (atom nil))
@@ -22,3 +25,16 @@
 (defn str-write [c]
   (swap! str-out str c)
   str-write)
+
+(defmethod getchar :test-device [device]
+ (let [input (:input device)
+       c (first @input)]
+   (if (nil? c)
+     :eof
+     (do
+       (swap! input rest)
+       c))))
+
+(defmethod putchar :test-device [device c]
+ (let [output (:output device)]
+   (swap! output str c)))
