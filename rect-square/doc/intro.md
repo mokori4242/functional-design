78d243f12e1762b23827e4718fb0bfa9ca64f82e
mokori4242 <mokori4242@gmail.com>
2024-08-12 09:49:42 +0900

    [update]LSP~長方形と正方形

---
+++ rect-square/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rect-square
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ rect-square/test/rect_square/core_test.clj
@@ -0,0 +1,7 @@
+(ns rect-square.core-test
+  (:require [clojure.test :refer :all]
+            [rect-square.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
