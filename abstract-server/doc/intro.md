35f5c2b0aba96f283cf14b79df5ed445e3c83fa5
mokori4242 <mokori4242@gmail.com>
2024-09-20 18:45:48 +0900

    [add]Abstract Server

---
+++ abstract-server/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to abstract-server
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ abstract-server/test/abstract_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns abstract-server.core-test
+  (:require [clojure.test :refer :all]
+            [abstract-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
